b44083d89aa78a3cbb11f605e9e21db4ae718da5
UIDevloper1 <sohil@evvoiot.com>
2023-12-04 21:54:38 +0530

    comit

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
         <GlobalStyle/>
           <Nav/>
           <Routes>
-            <Route path="/" element={<Home/>} />
+            <Route path="/DAPTAR" element={<Home/>} />
             <Route path="/about" element={<About/>} />
             <Route path="/contact" element={<Contact/>} />
             <Route path="/products" element={<Product/>} />
